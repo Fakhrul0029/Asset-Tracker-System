34e9b0997bed0b5a022465ae922743b8b2ebb7fd
Fakhrul0029 <kulaiman.herman@gmail.com>
2026-05-05 09:21:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def add():
 
     add_log(asset_id, "Asset Created")
 
-    base_url = "https://your-app-name.onrender.com"
+    base_url = "https://asset-tracker-system-jg9d.onrender.com/dashboard"
     url = f"{base_url}/asset/{asset_id}"
 
     qr = qrcode.make(url)
